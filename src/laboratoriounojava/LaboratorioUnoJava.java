c72085b358cde7e5a9830dd5f5654c3fd398baa1
godpoh <jeremy12troll@gmail.com>
2024-05-30 14:47:55 -0600

    Finalizado.

--- src/laboratoriounojava/LaboratorioUnoJava.java
+++ src/laboratoriounojava/LaboratorioUnoJava.java
@@ -4,6 +4,9 @@
  */
 package laboratoriounojava;
 
+import java.util.Scanner;
+import java.util.HashMap;
+
 /**
  *
  * @author Admin
@@ -14,7 +17,133 @@ public class LaboratorioUnoJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        menu();
     }
-    
+
+    public static void menu() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Menu");
+            System.out.println("1. Registro de personas.");
+            System.out.println("2. Registro de Productos.");
+            System.out.println("3. Busqueda y Compra de Productos.");
+            System.out.println("4. Reportes.");
+            System.out.println("5. Salir");
+
+            if (scanner.hasNextInt()) {
+                int optionMenu = scanner.nextInt();
+                switch (optionMenu) {
+                    case 1:
+                        registrationOfPeople();
+                        break;
+                    case 2:
+                        registrationOfProducts();
+                        break;
+                    case 3:
+                        searchNBuyOfProducts();
+                        break;
+                    case 4:
+                        reports();
+                        break;
+                    case 5:
+                        System.out.println("Saliendo del programa...");
+                        System.exit(0);
+                    default:
+                        System.out.println("Error: Ingrese una opcion valida. Intentelo de nuevo.");
+                }
+            } else {
+                scanner.next();
+                System.out.println("Error: No se permite ingresar letras. Intentelo de nuevo.");
+            }
+        }
+    }
+
+    public static void registrationOfPeople() {
+        Scanner scanner = new Scanner(System.in);
+
+        HashMap<Integer, HashMap<String, Object>> indexDictionary = new HashMap<>();
+
+        System.out.println("Registro de Personas");
+
+        while (true) {
+            System.out.println("Ingrese su cedula: ");
+            int idNumber = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Ingrese su nombre: ");
+            String name = scanner.nextLine();
+
+            String gender;
+            while (true) {
+                System.out.println("Ingrese su genero: ");
+                System.out.println("1. Masculino");
+                System.out.println("2. Femenino");
+
+                if (scanner.hasNextInt()) {
+                    int optionsGender = scanner.nextInt();
+                    scanner.nextLine(); 
+                    if (optionsGender == 1) {
+                        gender = "Masculino";
+                        break;
+                    } else if (optionsGender == 2) {
+                        gender = "Femenino";
+                        break;
+                    } else {
+                        System.out.println("Ingrese una opcion valida. Intentelo de nuevo");
+                    }
+                } else {
+                    scanner.nextLine();
+                    System.out.println("Error: Ingrese un numero valido.");
+                }
+            }
+
+            HashMap<String, Object> personInfo = new HashMap<>();
+            personInfo.put("Cedula", idNumber);
+            personInfo.put("Nombre", name);
+            personInfo.put("Genero", gender);
+
+            int nextIndex = indexDictionary.size() + 1;
+
+            indexDictionary.put(nextIndex, personInfo);
+
+            System.out.println("Informacion registrada:");
+            for (int i = 1; i <= indexDictionary.size(); i++) {
+                HashMap<String, Object> currentPersonInfo = indexDictionary.get(i);
+                System.out.println("Persona " + i + ":");
+                System.out.println("Cedula: " + currentPersonInfo.get("Cedula"));
+                System.out.println("Nombre: " + currentPersonInfo.get("Nombre"));
+                System.out.println("Genero: " + currentPersonInfo.get("Genero"));
+                System.out.println();
+            }
+            while (true) {
+                System.out.println("Desea agregar otra persona?: ");
+                System.out.println("Si ingresa un valor que no sea el indicado se le devolvera al menu principal.");
+                System.out.println("1. Si");
+                System.out.println("2. No");
+
+                if (scanner.hasNextInt()) {
+                    int continueInput = scanner.nextInt();
+                    if (continueInput != 2) {
+                        break;
+                    }
+                } else {
+                    System.out.println("Error: Ingrese un numero valido");
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void registrationOfProducts() {
+
+    }
+
+    public static void searchNBuyOfProducts() {
+
+    }
+
+    public static void reports() {
+
+    }
 }
